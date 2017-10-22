d1c407ab4084b8381f861df507c9b5dca9091b64
wgparish <williamparish159@gmail.com>
2017-10-22 17:31:48 -0400

    updated script

--- Init_Setup/bestjudge.sql
+++ Init_Setup/bestjudge.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 22, 2017 at 08:25 PM
+-- Generation Time: Oct 22, 2017 at 11:30 PM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.9
 
@@ -51,9 +51,17 @@ CREATE TABLE `rubric_descriptor` (
   `rubric_identifier` int(55) NOT NULL,
   `static_point_value` double NOT NULL,
   `descriptor` text NOT NULL,
-  `score_sheet` enum('notebook','marketing','exhibit_and_interview','spirit_and_sportsmanship','robot_performance') NOT NULL,
+  `score_sheet` enum('Notebook','Marketing','Exhibit_and_interview','Spirit_and_sportsmanship','Robot') NOT NULL,
   `descriptor_title` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `rubric_descriptor`
+--
+
+INSERT INTO `rubric_descriptor` (`id`, `rubric_identifier`, `static_point_value`, `descriptor`, `score_sheet`, `descriptor_title`) VALUES
+  (1, 1, 10, '8-10\r\nIn-depth discussion of how this year’s game theme relates to industry in this region.\r\n4-7\r\nGeneral discussion of how this year’s game theme relates to industry in this region.\r\n1-3\r\nGeneral discussion of this year’s game theme, but no ties to the region.\r\n0\r\nNo discussion of this year’s game theme.', 'Notebook', 'Correlation between the game and how the science/technology is being used at a company/industry/research lab in the team’s state or region'),
+  (2, 2, 10, '8-10\r\nIn-depth discussion of history and people related to this year’s game theme.\r\n4-7\r\nGeneral discussion of history and people related to this year’s game theme.\r\n1-3\r\nSome mention of history or people.\r\n0\r\nNo discussion of this year’s game theme.', '', 'Any related information of the game theme such as history, famous inventor(s), major milestones, etc.');
 
 -- --------------------------------------------------------
 
@@ -64,10 +72,22 @@ CREATE TABLE `rubric_descriptor` (
 CREATE TABLE `score_sheet` (
   `id` int(55) NOT NULL,
   `team_number` int(55) NOT NULL,
-  `score_sheet` enum('notebook','marketing','exhibit_and_interview','spirit_and_sportsmanship','robot_performance') NOT NULL,
+  `score_sheet` enum('Notebook','Marketing','Exhibit_and_interview','Spriri_and_sportsmanship','Robot') NOT NULL,
   `point_value` double NOT NULL,
   `judge_number` int(55) NOT NULL,
   `rubric_identifier` int(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `settings`
+--
+
+CREATE TABLE `settings` (
+  `id` varchar(200) NOT NULL,
+  `label` varchar(200) NOT NULL,
+  `value` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -101,20 +121,27 @@ CREATE TABLE `team` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_table`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `user_table` (
+CREATE TABLE `users` (
   `id` int(55) NOT NULL,
   `judge_number` int(55) NOT NULL,
   `accessLevel` enum('admin','head_judge','judge','restricted') NOT NULL,
   `username` text NOT NULL,
   `password` text NOT NULL,
-  `email_address` text NOT NULL,
+  `email` text NOT NULL,
   `first_name` text NOT NULL,
   `last_name` text NOT NULL,
   `email_verification` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `judge_number`, `accessLevel`, `username`, `password`, `email`, `first_name`, `last_name`, `email_verification`) VALUES
+  (1, 1, 'admin', 'wparish', '$2y$12$NMHj3R56Vw/m13c9q.PzMOzpDShVrX.T1eP5zU8DYb3o8K57.2ntu', 'williamparish159@gmail.com', 'William', 'Parish', 1);
 
 --
 -- Indexes for dumped tables
@@ -145,9 +172,9 @@ ALTER TABLE `team`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `user_table`
+-- Indexes for table `users`
 --
-ALTER TABLE `user_table`
+ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -164,7 +191,7 @@ ALTER TABLE `modify`
 -- AUTO_INCREMENT for table `rubric_descriptor`
 --
 ALTER TABLE `rubric_descriptor`
-  MODIFY `id` int(55) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(55) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `score_sheet`
@@ -179,10 +206,10 @@ ALTER TABLE `team`
   MODIFY `id` int(55) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `user_table`
+-- AUTO_INCREMENT for table `users`
 --
-ALTER TABLE `user_table`
-  MODIFY `id` int(55) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `users`
+  MODIFY `id` int(55) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- judge.php
+++ judge.php
@@ -28,5 +28,39 @@ include(D_TEMPLATE . '/header.php');
 </div>
 
 <?php
+global $DB;
+$q = $DB->query('SELECT * FROM rubric_descriptor');
+while($r = $q->fetch()){
+    ?>
+<section>
+    <div class="container py-3">
+        <div class="card">
+            <div class="row ">
+                <div class="col-md-8 px-3">
+                    <div class="card-block px-3">
+                        <h4 class="card-title"><?php echo $r['descriptor_title']; ?></h4>
+                        <h6 class="card-title"><?php echo $r['static_point_value']; ?></h6>
+                        <p class="card-text"><?php echo $r['descriptor']; ?></p>
+                    </div>
+                </div>
+                <div class="col-md-4 px-3">
+                    <br/>
+                    <br/>
+                    <form class="form-control" method="post" action="judge.php">
+                        <input type="text" class="form-control" placeholder="insertOldValueHere">
+                        <br/>
+                        <button type="submit" class="btn btn-success">Save</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+}
+?>
+
+
+<?php
 include(D_TEMPLATE . '/footer.php');
 ?>
