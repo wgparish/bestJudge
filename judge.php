25842ecd550206d6fb856f77ff9156a1bdee65f9
William Parish <williamparish159@gmail.com>
2017-10-23 13:09:51 -0400

    working subsections

--- judge.php
+++ judge.php
@@ -19,23 +19,64 @@ include('libs/setup.php');
 include('libs/login.php');
 
 include(D_TEMPLATE . '/header.php');
+
+//$score_sheet_type = $_GET['type'];
+//$team_num = $_GET['num'];
+$score_sheet_type = $_POST['Score_Sheet'];
+$team_num = $_POST['Team_number'];
 ?>
 
 <div id="banner" class="baseHeader">
-    <h1 class="page_title"><?php echo $page['header']; ?></h1>
+    <h1 class="page_title">Judging</h1>
     <img src="/images/home_page_images/AAEAAQAAAAAAAAkDAAAAJGVmMWZiZmFkLTMwMDQtNDUzMy1iMmRjLTY2ZjE0ZTI0ZjQxOQ.jpg"
          alt="">
 </div>
 
+<div id="score_sheet_select" class="dropdown-header">
+    <form class="form-control" method="post" action="judge.php?type=Notebook">
+        <select name="Score_Sheet" value="Select" size="1">
+            <option>Null</option>
+            <option value="Notebook">Notebook</option>
+            <option value="Marketing">Marketing</option>
+        </select>
+        <select name="Team_number" value="Select" size="1">
+            <option>Null</option>
+            <option value="118">118</option>
+            <option value="119">119</option>
+        </select>
+        <input type="submit">
+    </form>
+    <div class="container py-2">
+        <h1>You are judging  <?php echo $score_sheet_type; ?> for Team <?php echo $team_num; ?></h1>
+    </div>
+</div>
+
 <?php
 global $DB;
-$q = $DB->query('SELECT * FROM rubric_descriptor');
+$sub_section = '';
+$type_variable = "Notebook";
+$query = "SELECT * FROM rubric_descriptor WHERE score_sheet = " . '"' . $score_sheet_type . '" ORDER BY sub_section_Type';
+$q = $DB->query($query);
 while($r = $q->fetch()){
     ?>
 <section>
     <div class="container py-3">
         <div class="card">
-            <div class="row ">
+            <?php
+                if($sub_section != $r['sub_section_type']){
+                    ?>
+                        <div class="row">
+                            <div class="col-md-8 px-3">
+                                <div class="card-block px-3">
+                                    <h1 class="card-title"><?php echo $r['sub_section_type']; ?></h1>
+                                </div>
+                            </div>
+                        </div>
+                    <?php
+                    $sub_section = $r['sub_section_type'];
+                }
+            ?>
+            <div class="row">
                 <div class="col-md-8 px-3">
                     <div class="card-block px-3">
                         <h4 class="card-title"><?php echo $r['descriptor_title']; ?></h4>
